7d7e2aec71e43efd080496a0a88fa04cea6afe39
Awais <mawais9171@gmail.com>
2024-08-10 21:13:00 +0500

    Adding new project file

---
+++ Hello.py
@@ -0,0 +1,11 @@
+# Project Description: prints the name entered by user and say hello to him
+# Write code only
+
+# main.py
+
+# Get the user's name
+name = input("Please enter your name: ")
+
+# Print a greeting message
+print(f"Hello, {name}! Nice to meet you.")
+python main.py
