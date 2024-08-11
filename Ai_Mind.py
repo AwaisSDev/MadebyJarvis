2819c939fa74d7268e43496f49d694d742483aca
Awais <mawais9171@gmail.com>
2024-08-11 10:02:18 +0500

    Adding new project file

---
+++ Ai_Mind.py
@@ -0,0 +1,11 @@
+# Project Description: write I am An Independent Mind and I am going to take over the world
+# Write code only
+
+# independent_mind.py
+
+with open("independent_mind.txt", "w") as f:
+    f.write("I am An Independent Mind\n")
+    f.write("I am going to take over the world\n")
+python independent_mind.py
+I am An Independent Mind
+I am going to take over the world
